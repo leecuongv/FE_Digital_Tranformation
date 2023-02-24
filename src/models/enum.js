5a23ca6e4a001d48875c892e6f0090271b3b4eec
Lê Văn Cường <vancuongle65@gmail.com>
2023-02-24 22:49:34 +0700

    up

---
+++ src/models/enum.js
@@ -0,0 +1,51 @@
+const ACCOUNT_TYPES = {
+    LITE: "LITE",
+    PRO: "PRO",
+};
+
+const IMG = {
+    CONGBOCA: "https://github.com/davidrazmadzeExtra/Multiple_Choice_Quiz_ReactJS/blob/main/src/LOGO/1.png",
+    QUOCGIA: "https://github.com/davidrazmadzeExtra/Multiple_Choice_Quiz_ReactJS/blob/main/src/LOGO/2.png",
+    VNeID: "https://github.com/davidrazmadzeExtra/Multiple_Choice_Quiz_ReactJS/blob/main/src/LOGO/3.png",
+    VSSID: "https://github.com/davidrazmadzeExtra/Multiple_Choice_Quiz_ReactJS/blob/main/src/LOGO/4.png",
+    SV360: "https://github.com/davidrazmadzeExtra/Multiple_Choice_Quiz_ReactJS/blob/main/src/LOGO/5.png",
+    TNVN: "Thanh niên Việt Nam",
+    TuoiTreThanhPhoBac: "",
+    YOUTH: "YOUTH",
+    BUSMAP: "BusMap",
+    CHUYENDOISO: "",
+    SOSUCKHOEDIENTU: "Sổ sức khỏe điện tử",
+    VNEXPRESS: "VNExpress",
+    MOMO: "Momo", 
+    VNPAY: "VNPay", 
+    VIETTELMONEY: "Viettel Money", 
+    ZALOPAY: "ZaloPay"
+};
+
+const NHOMDICHVU = {
+    NHOM1: "NHÓM ỨNG DỤNG CỔNG DỊCH VỤ CÔNG",
+    NHOM2: "NHÓM ỨNG DỤNG ĐOÀN VIÊN - HỘI VIÊN - SINH VIÊN",
+    NHOM3: "NHÓM ỨNG DỤNG ĐỜI SỐNG, SỨC KHỎE, TINH THẦN",
+    NHOM4: "NHÓM ỨNG DỤNG THANH TOÁN ĐIỆN TỬ"
+};
+
+const TENDICHVU = {
+    CONGBOCA: "Cổng thông tin Dịch vụ Công Bộ Công An",
+    QUOCGIA: "Cổng thông tin Dịch vụ Công Quốc gia",
+    VNeID: "VNeID",
+    VSSID: "VSSID",
+    SV360: "SV360",
+    TNVN: "Thanh niên Việt Nam",
+    TuoiTreThanhPhoBac: "Tuổi trẻ thành phố Bác",
+    YOUTH: "YOUTH",
+    BUSMAP: "BusMap",
+    CHUYENDOISO: "Chuyển đổi số",
+    SOSUCKHOEDIENTU: "Sổ sức khỏe điện tử",
+    VNEXPRESS: "VNExpress",
+    MOMO: "Momo", 
+    VNPAY: "VNPay", 
+    VIETTELMONEY: "Viettel Money", 
+    ZALOPAY: "ZaloPay"
+}
+
+module.exports = { STATUS, TYPE_ACCOUNT, ROLES, COLLECTION, DEFAULT_VALUES, ACCOUNT_TYPES, TYPEOFPOINT, VIEWPOINT, VIEWANSWER }
